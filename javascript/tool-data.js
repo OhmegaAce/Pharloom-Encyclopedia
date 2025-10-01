1b82fbfde9876de09bef347c8bffd6b9fc1c21af
Kole Fiala <kolefiala@gmail.com>
2025-09-30 20:50:45 -0500

    - added more red tools

--- javascript/tool-data.js
+++ javascript/tool-data.js
@@ -35,6 +35,61 @@ const toolData = {
       uses: "5/6/7/8/10",
       refill_cost: 8,
     },
+    {
+      name: "Longpin",
+      image: "../images/Tools/red/longpin.png",
+      description: "Weighted pin and haft, formed to pierce tough shells.",
+      location:
+        "Located in Shellwood above the main entrance to Bellhart, blocked by a Wood Wasp nest.",
+      uses: "10/12/15/17/20",
+      refill_cost: 4,
+    },
+    {
+      name: "Curveclaw",
+      image: "../images/Tools/red/curveclaw.png",
+      description: "Arched sharpbone used by hunters to take down flying prey.",
+      location:
+        "Purchased from Mottled Skarr in Hunter's March for  140. Alternatively, if Mottled Skarr is dead it can be found locked behind a gauntlet north of his original location.",
+      uses: "8/10/12/14/16",
+      refill_cost: 5,
+    },
+    {
+      name: "Curvesickle",
+      image: "../images/Tools/red/curvesickle.png",
+      description:
+        "Radial sharpbone used by skilled hunters to take down dangerous prey.",
+      location:
+        "Obtained by giving the Curveclaw to a Mottled Skarr Scout in Act 3, in the target practice room above the Far Fields Bellway station.",
+      uses: "8/10/12/14/16",
+      refill_cost: 5,
+    },
+    {
+      name: "Throwing Ring",
+      image: "../images/Tools/red/throwingring.png",
+      description:
+        "Hardened throwing weapon of the warrior Shakra. Blunt force weapon that rebounds between enemies and hard surfaces.",
+      location: "Reward for completing the Trail's End Wish.",
+      uses: "10/12/15/17/20",
+      refill_cost: 4,
+    },
+    {
+      name: "Pimpillo",
+      image: "../images/Tools/red/pimpillo.png",
+      description:
+        "Fabric pouch filled with volatile material. Explodes on impact with enemies or when ignited by its fuse.",
+      location: "Crafted in a hidden room in northwest Greymoor for  1.",
+      uses: "3/3/4/5/6",
+      refill_cost: 13.33,
+    },
+    {
+      name: "Conchcutter",
+      image: "../images/Tools/red/conchcutter.png",
+      description:
+        "Spiral weapon fashioned from a hardened conchspire. Thrown at an angle, it can ricochet and catch prey off-guard.",
+      location: "Found inside the Coral Tower in Sands of Karak.",
+      uses: "8/10/12/14/16",
+      refill_cost: 5,
+    },
   ],
   bluetools: [
     {
