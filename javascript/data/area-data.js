469949990ae0ebcc0c9c2ec223b1d1969ea42d24
Kole Fiala <kolefiala@proton.me>
2025-10-03 15:29:49 -0500

    - started adding information to area-data.js - finished added pictures for the areas - started adding music themes for the areas

--- javascript/data/area-data.js
+++ javascript/data/area-data.js
@@ -1,13 +1,266 @@
 const areaData = {
   areas: [
     {
-      name: "",
+      name: "Bellheart",
+      information:
+        "Bellhart is a town and an area in Hollow Knight: Silksong. Entering it for the first time will trigger the Main Objective SAVE: The Threadspun Town.",
+      description:
+        "When first entering, the town is called Haunted Bellhart, its residents are strung in silk threads hanging from the ceiling and all buildings except for the Bellway are closed. After Widow is defeated and Bellhart is freed, NPCs and shops will start to appear in the bellhomes, as well as a Wishwall. A new bell will appear at the top of the area after completing the Bellhart's Glory Wish and 2 of the following 4 Wishes: Silver Bells, My Missing Courier, My Missing Brother, Savage Beastfly. The bell is given to Hornet by the residents of Bellhart and can be unlocked with the Bellhome Key from Pavo.",
+      image: "../images/Areas/bellheart.png",
+      theme: "Bellheart",
+      music: "../music/areas/bellheart.mp3",
+    },
+    {
+      name: "Bilewater",
+      information:
+        "Bilewater is an area in Hollow Knight: Silksong. It is characterised by a dark green hue and its murky, swamp-like rooms.",
+      description:
+        "Bilewater is made up of dank tunnels overgrown with roots and thorns. Its waters and caves are poisoned by waste from the Citadel above. The far eastern end of Bilewater, below the Pale Lake, is the only area where the water has not been tainted.",
+      image: "../images/Areas/bilewater.png",
+      theme: "Bilewater",
+      music: "../music/areas/bilewater.mp3",
+    },
+    {
+      name: "Blasted Steps",
+      information: "The Blasted Steps are an area in Hollow Knight: Silksong. ",
+      description:
+        "The Blasted Steps connect lower Pharloom with The Citadel. The area is made up of natural caves through which a road to the Citadel has been built. Strong winds and sand continually batter the Blasted Steps, eroding much of the construction. The Steps are located at the edge of Pharloom, bordering the wasteland.",
+      image: "../images/Areas/blastedsteps.png",
+      theme: "Blasted Steps",
+      music: "../music/areas/blastedsteps.mp3",
+    },
+    {
+      name: "Bone Bottom",
+      information:
+        "Bone Bottom is the first town in Hollow Knight: Silksong that can be encountered. It is found in the Moss Grotto.",
+      description:
+        "Bone Bottom is a small town that leads to The Marrow. It is inhabited by pilgrims, whose objective is to reach the Citadel. It is composed of tents and bone pieces. Bone Bottom introduces the Tasks system, with a Wishwall that gets continuously updated over the course of the game. The wishwall is maintained by Flick the Fixer, and gets reconstructed after defeating the Bell Beast. The town also has a small shop owned by Pebb, along with a Bellway Station. Inside the Station resides the Fearful Pilgrim, who leaves the area after Hornet visits the town using the Bell Beast. The town has a bench, where the Pilby will sit beside and talk to Hornet about various areas as she unlocks them. After certain game events, Pilby will leave his usual spot and be replaced by Quiet Pilgrim.",
+      image: "../images/Areas/bonebottom.png",
+      theme: "Bone Bottom",
+      music: "../music/areas/bonebottom.mp3",
+    },
+    {
+      name: "Choral Chambers",
+      information:
+        "The Choral Chambers are an area in The Citadel in Hollow Knight: Silksong.",
+      description: "Missing information",
+      image: "../images/Areas/choralchambers.png",
+      theme: "Choral Chambers",
+      music: "../music/areas/choralchambers.mp3",
+    },
+    {
+      name: "Cogwork Core",
+      information:
+        "The Cogwork Core is an area in The Citadel in Hollow Knight: Silksong.",
+      description:
+        "The Cogwork Core serves as the heart of The Citadel. This area also feature Cogwork enemies.",
+      image: "../images/Areas/cogworkcore.png",
+      theme: "Cogwork Core",
+      music: "../music/areas/cogworkcore.mp3",
+    },
+    {
+      name: "Deep Docks",
+      information:
+        "Deep Docks is an industrialized forge Area in Hollow Knight: Silksong.",
+      description:
+        "True to its name, it provides platforms and docks for lakes of lava. Scattered around the area are many metal carapaces used as containers for hot coals also found in the area. Its main inhabitants are forge workers who wear bell armour that shield them from falling rubble and also attacks from above. The lowest reaches of the Deep Docks houses a diving bell that Hornet uses to access The Abyss once she finds someone who can operate it.",
+      image: "../images/Areas/deepdocks.png",
+      theme: "Deep Docks",
+      music: "../music/areas/deepdocks.mp3",
+    },
+    {
+      name: "Far Fields",
+      information: "The Far Fields are an area in Hollow Knight: Silksong.",
+      description: "Missing information",
+      image: "../images/Areas/farfields.png",
+      theme: "Far Fields",
+      music: "../music/areas/farfields.mp3",
+    },
+    {
+      name: "Grand Gate",
+      information:
+        "The Grand Gate is an area in The Citadel in Hollow Knight: Silksong. ",
+      description:
+        "The Grand Gate is the first area reached of the Citadel and the start of Act 2. It is a large, elegant hall leading to an elaborate lift. The lift is dilapidated and collapses into the Underworks instead of functioning as intended. The main hall of the Grand Gate consists of several scales that can be used as platforms to climb towards the Citadel. These scales can be tipped either by standing on the elevated ends or striking them from below with an upward attack. In Act 3, exploration of the broken lift shaft allows Hornet to discover a passage that leads to a monument dedicated to Nyleth, guarded by Seth.",
+      image: "../images/Areas/grandgate.png",
+      theme: "Missing Information",
+      music: "",
+    },
+    {
+      name: "Greymoor",
+      information:
+        "Greymoor is a large town in Hollow Knight: Silksong, enclosed in a partly flooded cave system which constantly rains with groundwater. The misted fields and dilapidated towers are infested with Crawbugs, and the former tenders of the land have become haunted. Greymoor is one of the largest areas Team Cherry has created. It resembles the size and aesthetics of the City of Tears, its closest equivalent in Hollow Knight.",
+      description: "Missing Information",
+      image: "../images/Areas/greymoor.png",
+      theme: "Greymoor",
+      music: "../music/areas/greymoor.mp3",
+    },
+    {
+      name: "High Halls",
+      information:
+        "The High Halls are an area in The Citadel in Hollow Knight: Silksong which are inhabited by the conductors.",
+      description: "Missing Information",
+      image: "../images/Areas/highhalls.png",
+      theme: "High Halls",
+      music: "../music/areas/highhalls.mp3",
+    },
+    {
+      name: "Hunter's March",
+      information: "The Hunter's March is an area in Hollow Knight: Silksong.",
+      description:
+        "Hunter's March is the main territory of the Skarr tribe, and as a result, is full of ant enemies. There are also tiny ants that, while seemingly just decorative, are capable of carrying and moving corpses around.",
+      image: "../images/Areas/huntersmarch.png",
+      theme: "Hunter's Trail",
+      music: "../music/areas/hunterstrail.mp3",
+    },
+    {
+      name: "The Marrow",
+      information: "The Marrow is an area in Hollow Knight: Silksong.",
+      description: "Missing Information",
+      image: "../images/Areas/themarrow.png",
+      theme: "The Marrow",
+      music: "../music/areas/themarrow.mp3",
+    },
+    {
+      name: "Memorium",
+      information:
+        "The Memorium is an area in The Citadel in Hollow Knight: Silksong.",
+      description:
+        "The Memorium is a preserve located near the top of the Citadel, hosting specimens of various wildlife found from across Pharloom. It is separated into several floors, with rooms matching the native biomes of each specimen being housed there. The lowest floor represents Moss Grotto and The Marrow, the second floor represents the Far Fields, the third floor represents Shellwood, and the fourth floor represents Sands of Karak. The western side of Memorium was once host to species from Verdania, but it has been compromised from an offscreen disaster. The Verdania exhibits are ruined, its specimens dead. The top floor of the Memorium contains a Ventrica station for quick access.",
+      image: "../images/Areas/memorium.png",
+      theme: "Missing Information",
+      music: "",
+    },
+    {
+      name: "Moss Grotto",
+      information:
+        "The Moss Grotto is the first area reached in Hollow Knight: Silksong. It is located at the very bottom of Pharloom after the cinematic intro where Hornet breaks the cage she was trapped in and subsequently the bridge she was atop.",
+      description:
+        "Moss Grotto is a lush, verdant area overgrown with plant life.",
+      image: "../images/Areas/mossgrotto.png",
+      theme: "Moss Grotto",
+      music: "../music/areas/mossgrotto.mp3",
+    },
+    {
+      name: "Mount Fay",
+      information: "Mount Fay is an Area in Hollow Knight: Silksong.",
+      description:
+        "Mount Fay is nearly devoid of enemies to fight, and consists almost entirely of platforming challenges, requiring both Clawline and Drifter's Cloak to traverse. While in Mount Fay, Hornet takes damage from the cold if she is away from a heat source (campfires, lightbulbs, etc.) for too long, indicated by the Masks in her health bar quivering and the edges of the screen getting increasingly coated in ice. Caution should be exercised with certain heat sources, as they are destructible and can render certain sections impassable without resetting. Mount Fay also has frigid waters that rapidly sap away Hornet's health as she swims through it. The Ascendant's Grip, Weavelight, Flintslate, and Plasmium Phial Tools can all make the trek easier, though they aren't required. At the top of Mount Fay, Hornet unlocks the Faydown Cloak, which grants her immunity to frostbite from its cold weather. However, icy water remains a threat to her health.",
+      image: "../images/Areas/mountfay.png",
+      theme: "Mount Fay",
+      music: "../music/areas/mountfay.mp3",
+    },
+    {
+      name: "Putrified Ducts",
       information: "",
       description: "",
-      image: "",
+      image: "../images/Areas/putrifiedducts.png",
       theme: "",
       music: "",
     },
-    {},
+    {
+      name: "Sands of Karak",
+      information: "",
+      description: "",
+      image: "../images/Areas/sandsofkarak.png",
+      theme: "",
+      music: "",
+    },
+    {
+      name: "Shellwood",
+      information: "",
+      description: "",
+      image: "../images/Areas/shellwood.png",
+      theme: "",
+      music: "",
+    },
+    {
+      name: "Sinner's Road",
+      information: "",
+      description: "",
+      image: "../images/Areas/sinnersroad.png",
+      theme: "",
+      music: "",
+    },
+    {
+      name: "The Citadel",
+      information: "",
+      description: "",
+      image: "../images/Areas/thecitadel.png",
+      theme: "",
+      music: "",
+    },
+    {
+      name: "The Cradle",
+      information: "",
+      description: "",
+      image: "../images/Areas/thecradle.png",
+      theme: "",
+      music: "",
+    },
+    {
+      name: "The Slab",
+      information: "",
+      description: "",
+      image: "../images/Areas/theslab.png",
+      theme: "",
+      music: "",
+    },
+    {
+      name: "Underworks",
+      information: "",
+      description: "",
+      image: "../images/Areas/underworks.png",
+      theme: "",
+      music: "",
+    },
+    {
+      name: "Verdania",
+      information: "",
+      description: "",
+      image: "../images/Areas/verdania.png",
+      theme: "",
+      music: "",
+    },
+    {
+      name: "Weavenest Atla",
+      information: "",
+      description: "",
+      image: "../images/Areas/weavenestatla.png",
+      theme: "",
+      music: "",
+    },
+    {
+      name: "Whispering Vaults",
+      information: "",
+      description: "",
+      image: "../images/Areas/whisperingvaults.png",
+      theme: "",
+      music: "",
+    },
+    {
+      name: "Whiteward",
+      information: "",
+      description: "",
+      image: "../images/Areas/whiteward.png",
+      theme: "",
+      music: "",
+    },
+    {
+      name: "Wisp Thicket",
+      information: "",
+      description: "",
+      image: "../images/Areas/wispthicket.png",
+      theme: "",
+      music: "",
+    },
+    {
+      name: "Wormways",
+      information: "",
+      description: "",
+      image: "../images/Areas/wormways.png",
+      theme: "",
+      music: "",
+    },
   ],
 };
